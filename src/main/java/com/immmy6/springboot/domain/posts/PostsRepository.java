12ceeb9fba5f7370a3d0c48c77a07d69ef710a58
yoo-jaein <y_jaein@naver.com>
2020-11-15 22:55:17 +0900

    4장 글 작성까지

---
+++ src/main/java/com/immmy6/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,8 @@
+package com.immmy6.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
---
+++ src/main/java/com/immmy6/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.immmy6.springboot.web.dto;
+
+import com.immmy6.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
