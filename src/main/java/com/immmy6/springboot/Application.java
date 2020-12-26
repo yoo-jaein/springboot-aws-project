d332d99c23e008e324674d6365b78591363e2e62
yoo-jaein <y_jaein@naver.com>
2020-12-26 23:12:57 +0900

    index 수정 중

--- src/main/java/com/immmy6/springboot/Application.java
+++ src/main/java/com/immmy6/springboot/Application.java
@@ -2,9 +2,10 @@ package com.immmy6.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@EnableJpaAuditing
 //@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+//@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
